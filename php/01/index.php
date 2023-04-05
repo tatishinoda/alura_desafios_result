792f0f4331a4c0c8a558e6168c08070eb2e54023
Fabrizio Feitosa <fabriziofeitosa@live.com>
2023-04-05 14:45:22 -0300

    Estrutura PHP 01

---
+++ php/01/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Praticando PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Contando de 1 a 10</h1>
+        <p>Quem nunca contou até 10 né?</p>
+        <hr>
+        <div class="mt-3" id="saida"></div>
+    </div>
+
+    <style>
+        .container {
+            padding-top: 1rem;
+            max-width: 600px;
+        }
+    </style>
+</body>
+
+</html>
